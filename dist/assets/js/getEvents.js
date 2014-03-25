ec8cde25da4331b3d6e5aa6118672c2e078846ea
Diogo Moretti <diogo.pearljam@gmail.com>
2014-03-25 14:05:34 -0300

    Fix each/limit/count events

--- dist/assets/js/getEvents.js
+++ dist/assets/js/getEvents.js
@@ -1,12 +1,11 @@
 $.ajax({
-    url: "//query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%27http%3A%2F%2Fwww.unisul.br%2Fwps%2Fportal%2Fhome%2Ffique-por-dentro%2Feventos%2Fapi-50anos%27%20and%20xpath%3D%27%2F%2F*%5B%40id%3D%22eventos-50anos%22%5D%27&format=json&diagnostics=true&callback=?",
+    url: 'http://query.yahooapis.com/v1/public/yql?q=select * from html where url=\'http://www.unisul.br/wps/portal/home/fique-por-dentro/eventos/api-50anos\' and xpath=\'//*[@id="eventos-50anos"]\'&format=json&diagnostics=true&callback=?',
     dataType: 'jsonp',
     success: function (response) {
 
         var events = response.query.results.div.div;
         var getEventsHTML = '';
-
-        console.log('EVENTOS: ' + events)
+        var count = 0;
 
         $.each(events, function(i, value) {
 
@@ -15,18 +14,18 @@ $.ajax({
             var year         = events[i].span;
             var date         = day + '/' + month;
             var title        = events[i].h3;
-            var completeDate = new Date (month + ' ' + day + ", " + year);
+            var completeDate = new Date(month + ' ' + day + ', ' + year);
             var url          = year + '/' + month + '/' + events[i].h6;
 
             if (completeDate.getTime() > (new Date().getTime())) {
+                count++;
                 getEventsHTML += '<div class="event-box"><div class="event-item"><span class="event-date">' + date + '</span><h2 class="event-title"><a href="http://www.unisul.br/wps/portal/home/fique-por-dentro/eventos/todos/' + url + '" target="_blank">' + title + '</a></h2></div></div>';
+                return count < 4;
             }
-
-            return i < 4;
 
         });
 
-        $("#events-list").html(getEventsHTML);
+        $('#events-list').html(getEventsHTML);
         
     }
 });
